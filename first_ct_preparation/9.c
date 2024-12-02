4171fc6c6d58d7e10b63650149c8b7910d833b8c
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-12-02 23:22:18 +0600

    Added: Print all prime numbers within a given range.

---
+++ first_ct_preparation/9.c
@@ -0,0 +1,34 @@
+// Print all prime numbers within a given range.
+
+#include <stdio.h>
+
+int isPrime(int number){
+    if (number <= 1) {
+        return 0;
+    }
+    for(int i=2; i<=number; i++){
+
+        if(number % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+int main(){
+    int start, end;
+
+    printf("Enter the range (start and end): ");
+    scanf("%d %d", &start, &end);
+
+    printf("Prime numbers between %d and %d are:\n", start, end);
+
+
+    for(int number = start; number <= end; number++){
+        if(isPrime(number)){
+            printf("%d ", number);
+        }
+    }
+
+}
