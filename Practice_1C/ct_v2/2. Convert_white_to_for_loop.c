093b1893f4530e879d5802f3e664bc5024d1f123
Dip <65559847+Dip-Barua@users.noreply.github.com>
2024-12-31 18:26:36 +0600

    Added : Conversoin of given while loop using a for loop

---
+++ Practice_1C/ct_v2/2. Convert_white_to_for_loop.c
@@ -0,0 +1,19 @@
+// Rewrite the given while loop using a for loop
+
+// #include <stdio.h>
+// int main() {
+//     for (int i = 1; i <= 10; i += 2) {
+//         printf("i is: %d\n", i);
+//     }
+//     return 0;
+// }
+
+
+
+#include <stdio.h>
+
+int main(){
+    for(int i=1; i<=10; i++){
+        printf("i is: %d\n", i);
+    }
+}
