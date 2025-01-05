cc3c17d0460f0283d8919022b6905ec1e5e33ab3
Dip <65559847+Dip-Barua@users.noreply.github.com>
2025-01-06 00:57:04 +0600

    Added : Solution of 3rd problem from CT-1

---
+++ Practice_1C/ct_v2/CT_V1_solve/3. Grade_using_switchcase.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    float marks;
+
+    printf("enter your marks: ");
+    scanf("%f", &marks);
+
+    int grade = (int)marks / 10;
+    if (marks > 99.5) {
+        printf("Great!");
+    } else {
+    switch (grade) {
+        case 10:
+        case 9:
+        case 8:
+            printf("Grade: A+\n");
+            break;
+        case 7:
+            printf("Grade: A\n");
+            break;
+        case 6:
+            printf("Grade: A-\n");
+            break;
+        case 5:
+            printf("Grade: B+\n");
+            break;
+        case 4:
+            printf("Grade: B\n");
+            break;
+        default:
+            if (marks >= 0 && marks < 40) {
+                printf("Grade: F\n");
+            } else {
+                printf("Invalid marks entered!\n");
+            }
+            break;
+    }}
+
+}
