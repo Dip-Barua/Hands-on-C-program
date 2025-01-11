8cff3399be8e73453ac553d4e554d808ac26aae1
Dip <65559847+Dip-Barua@users.noreply.github.com>
2025-01-12 01:44:51 +0600

    Added : C program to find the factorial of a natural number

---
+++ MidTerm_practice/Loop (32)/10. find profuct of natual number.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int n , sum =1 ;
+
+    printf("Enter the number : ");
+    scanf("%d", &n);
+
+    for(int i=1; i<=n; i++){
+        sum *= i;
+    }
+    
+    printf("Factorial of %d is : %d", n, sum);
+}
